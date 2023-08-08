3550df0d8d2c20e6b2055d961c66bde45ed808b8
RyotaHiraiwa <ryota.hiraiwa@qccs.co.jp>
2023-08-08 14:25:54 +0900

    [feat] ログイン画面実装

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={ <Login/> }/>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/pages/Login/index.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './index.scss';
+
+const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+  }
+
+  const handlePassword = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  }
+
+  const executeLogin = async () => {
+    const response = await fetch('http://localhost:5000/login', {
+      credentials:'include',
+      mode: "cors",
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+
+    const json = await response.json()
+
+    if (json.result !== true) {
+      alert('ユーザー名またはパスワードが違います');
+      return;
+    }
+
+    navigate('/WeightInput');
+  }
+  
+  return (
+    <div className='login'>
+      <h2 className='login__title'>アプリケーションタイトル</h2>
+      <input className='login__input'
+             type='text'
+             value={ username }
+             onChange={ handleUsername }
+             placeholder='ユーザー名'/>
+      <input className='login__input'
+             type='password'
+             value={ password }
+             onChange={ handlePassword }
+             placeholder='パスワード'/>
+      <button className='login__button' onClick={ executeLogin }>ログイン</button>
+    </div>
+  );
+}
+
+export default Login;
