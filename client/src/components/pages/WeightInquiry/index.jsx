6dd1ed0a9b5fd7c934e736aa82b616fabe1e4851
rhiraiwa <kotomukeyawasu@outlook.jp>
2023-09-02 15:53:21 +0900

    [feat] データの期間選択機能追加

--- client/src/components/pages/WeightInquiry/index.jsx
+++ client/src/components/pages/WeightInquiry/index.jsx
@@ -2,23 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { baseUri } from '../../../const'
 import Header from '../../organisms/Header';
 import LineChartComponent from '../../templates/LineChartComponent';
-// import filter from '../../../img/filter_primary-blue.png';
-import PFCLineChart from '../../templates/PFCLineChart';
 import Footer from '../../organisms/Footer';
 import './index.scss';
 
 const WeightInquiry = () => {
   const [weight, setWeight] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
   
   const [activeTab, setActiveTab] = useState(0);
   const tabItems = [
-    { title: '体重', content: <LineChartComponent data={ weight } dataKey1='weight' dataKey2='weight_average' dataKey3='weight_goal'/> },
-    { title: '体脂肪率', content: <LineChartComponent data={ weight } dataKey1='bodyFatPercentage' dataKey2='BFP_average' dataKey3='BFP_goal'/> },
-    { title: 'PFC', content: <PFCLineChart data={ weight} /> }
+    { title: '体重', content: <LineChartComponent data={ displayData } dataKey1='weight' dataKey2='weight_average' dataKey3='weight_goal'/> },
+    { title: '体脂肪率', content: <LineChartComponent data={ displayData } dataKey1='bodyFatPercentage' dataKey2='BFP_average' dataKey3='BFP_goal'/> },
+    { title: 'PFC', content: <LineChartComponent data={ displayData } dataKey1='calorie' dataKey2='' dataKey3=''/> } 
   ];
 
   const handleTabClick = (index) => {
     setActiveTab(index);
+  };
+
+  const [selectedRadio, setSelectedRadio] = useState(0);
+  const radioButtons = ['過去２週間', '過去１ヶ月', '全データ'];
+
+  const handleRadioClick = (index) => {
+    setSelectedRadio(index);
   };
 
   useEffect(() => {
@@ -40,6 +46,42 @@ const WeightInquiry = () => {
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    // 今日の日付を取得
+    const today = new Date();
+    const todayISO = today.toISOString().slice(0, 10); // ISO形式の日付に変換
+  
+    // 期間の開始日を計算する関数
+    const calculateStartDate = (days) => {
+      const startDate = new Date(today);
+      startDate.setDate(today.getDate() - days); // 指定された日数を引く
+      return startDate.toISOString().slice(0, 10); // ISO形式の日付に変換
+    };
+  
+    const twoWeeksAgoISO = calculateStartDate(14);
+    const monthAgoISO = calculateStartDate(30);
+  
+    if (selectedRadio === 2) {
+      setDisplayData(weight);
+      return;
+    }
+  
+    // フィルタリング処理
+    const filteredData = weight.filter((item) => {
+      const itemDate = item.date;
+      switch (selectedRadio) {
+        case 0:
+          return itemDate >= twoWeeksAgoISO && itemDate <= todayISO;
+        case 1:
+          return itemDate >= monthAgoISO && itemDate <= todayISO;
+        default:
+          return false;
+      }
+    });
+  
+    setDisplayData(filteredData);
+  }, [weight, selectedRadio]);  
 
   return (
     <React.Fragment>
@@ -69,6 +111,17 @@ const WeightInquiry = () => {
             <input type='checkbox' />
             <label>測定値</label>
           </div> */}
+          <div className='filter__radio'>
+            {radioButtons.map((item, index) => (
+              <div
+                key={index}
+                className={`filter__radio-button ${index === selectedRadio ? 'active' : ''}`}
+                onClick={() => handleRadioClick(index)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <Footer/>
--- client/src/components/templates/LineChartComponent/index.jsx
+++ client/src/components/templates/LineChartComponent/index.jsx
@@ -1,48 +1,97 @@
+import { useEffect, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './index.scss';
 
+// ミリ秒から日付文字列に変換する関数
+const formatTimestamp = (timestamp) => {
+  const date = new Date(timestamp);
+  return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+};
+
 const LineChartComponent = ({ data, dataKey1, dataKey2, dataKey3 }) => {
+  // データを取得する前にDateオブジェクトに変換
+  const formattedData = data.map(item => ({
+    ...item,
+    date: new Date(item.date).getTime(), // 日付をミリ秒に変換
+    formattedDate: new Date(item.date).toISOString().split('T')[0], // 日付をフォーマット
+  }));
 
-  // const getAutoYDomain = (data, marginRatio = 0.1)  => {
-  //   const values = data.map(entry => entry.uv);
-  //   const minValue = Math.min(...values);
-  //   const maxValue = Math.max(...values);
-  //   const margin = (maxValue - minValue) * marginRatio;
-  //   return [minValue - margin, maxValue + margin];
-  // }
+  const [minX, setMinX] = useState(null);
+  const [maxX, setMaxX] = useState(null);
 
-  // const yDomain = getAutoYDomain(data);
+  useEffect(() => {
+    if (data.length === 0) return;
+
+    // 最小値と最大値の初期値を設定
+    let minDate = formattedData[0].date;
+    let maxDate = formattedData[0].date;
+
+    // データ内の最小値と最大値を計算
+    formattedData.forEach(item => {
+      minDate = Math.min(minDate, item.date);
+      maxDate = Math.max(maxDate, item.date);
+    });
+
+    // 3日前と3日後の日付を計算
+    const threeDays = 3 * 24 * 60 * 60 * 1000; // 3日分のミリ秒数
+    const startDate = minDate - threeDays;
+    const endDate = maxDate + threeDays;
+
+    // 範囲を設定
+    setMinX(startDate);
+    setMaxX(endDate);
+  }, [data, formattedData]);
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <LineChart data={data}
-      //   margin={{
-      //     top: 20,
-      //     right: 20,
-      //     bottom: 20,
-      //     left: 20,
-      // }}
-      >
-        <XAxis dataKey="date" />
-        {/* <YAxis className="y-axis" mirror={true}/> */}
+      <LineChart data={formattedData}>
+        <XAxis
+          dataKey="date"
+          domain={[minX, maxX]}
+          type="number"
+          tickFormatter={formatTimestamp} // X軸のティックを日付形式にフォーマット
+        />
         <YAxis className="y-axis" mirror={true} domain='auto' />
         <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
+        {
+          dataKey1 === 'calorie' ? (
+            <Tooltip content={CustomTooltip} active={data !== null} payload={data} />
+          ) : (
+            <Tooltip
+              labelFormatter={(value) => {
+                const date = new Date(value);
+                return date.toISOString().split('T')[0];
+              }}
+            />
+          )
+        }
         <Legend verticalAlign="bottom" />
         <Line type="monotone" dataKey={dataKey1} stroke="#a4c4e1" isAnimationActive={false} />
-        {
-          dataKey2 !== '' && (
-            <Line type="monotone" dataKey={dataKey2} stroke="#f1d6df" isAnimationActive={false} />
-          )
-        }
-        {
-          dataKey3 !== '' && (
-            <Line type="monotone" dataKey={dataKey3} stroke="#a4e1a9" isAnimationActive={false} dot={false}/>
-          )
-        }
+        {dataKey2 !== '' && (
+          <Line type="monotone" dataKey={dataKey2} stroke="#f1d6df" isAnimationActive={false} />
+        )}
+        {dataKey3 !== '' && (
+          <Line type="monotone" dataKey={dataKey3} stroke="#a4e1a9" isAnimationActive={false} dot={false}/>
+        )}
       </LineChart>
     </ResponsiveContainer>
   );
+};
+
+const CustomTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label-date">{`${formatTimestamp(payload[0].payload.date)}`}</p>
+        <p className="label-calorie">{`${payload[0].name} : ${payload[0].value}`}</p>
+        <p className="label-pfc">{`protein : ${payload[0].payload.protein}`}</p>
+        <p className="label-pfc">{`fat : ${payload[0].payload.fat}`}</p>
+        <p className="label-pfc">{`carbohydrate : ${payload[0].payload.carbohydrate}`}</p>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default LineChartComponent;
